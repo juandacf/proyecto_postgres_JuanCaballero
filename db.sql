b4584ab97cfc9a2a5b1af54c8118e54194d02a23
Juan Caballero <juandacf@gmail.com>
2025-04-11 08:17:25 -0500

    modelo funcional

--- db.sql
+++ db.sql
@@ -1,7 +1,6 @@
 
 DROP DATABASE IF EXISTS techzone0425;
 CREATE DATABASE techzone0425;
-\c techzone0425;
 
 
 CREATE TABLE IF NOT EXISTS categorias(
@@ -29,37 +28,37 @@ CREATE TABLE IF NOT EXISTS proveedores(
 
 CREATE TABLE IF NOT EXISTS ventas(
     id_venta SERIAL,
-    cedula_ciudadania DECIMAL(12,0),
+    id_cliente INTEGER,
     fecha DATE,
     PRIMARY KEY(id_venta),
-    FOREIGN KEY cedula_ciudadania REFERENCES clientes(cedula_ciudadania)
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
 
 CREATE TABLE IF NOT EXISTS productos (
 id_producto SERIAL,
 nombre_producto VARCHAR(20),
-id_categoria INTEGER(11),
+id_categoria INTEGER,
 precio BIGINT,
-stock_actual INTEGER
+stock_actual INTEGER,
 PRIMARY KEY(id_producto),
-FOREIGN KEY id_categoria REFERENCES categorias(id_categoria)
+FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
 );
 
 CREATE TABLE IF NOT EXISTS productos_proveedores (
     id_producto_proveedor SERIAL,
-    id_producto INTEGER(11),
-    id_proveedor INTEGER(11),
+    id_producto INTEGER,
+    id_proveedor INTEGER,
     PRIMARY KEY(id_producto_proveedor),
-    FOREIGN KEY id_producto REFERENCES productos(id_producto),
-    FOREIGN KEY id_proveedor REFERENCES proveedores(id_proveedor)
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
 );
 
 CREATE TABLE IF NOT EXISTS productos_ventas (
     id_producto_venta SERIAL,
-    id_venta  INTEGER(11),
-    id_producto INTEGER(11),
+    id_venta  INTEGER,
+    id_producto INTEGER,
     cantidad INTEGER,
     PRIMARY KEY(id_producto_venta),
-    FOREIGN KEY id_venta REFERENCES ventas(id_venta),
-    FOREIGN KEY id_producto REFERENCES productos(id_producto)
+    FOREIGN KEY (id_venta) REFERENCES ventas(id_venta),
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
